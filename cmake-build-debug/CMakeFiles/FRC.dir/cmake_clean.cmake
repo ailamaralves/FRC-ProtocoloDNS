eb82fd81f26320931fdd4a37479063ac8465923f
Ailamar Alves <ailamar.alvesg@gmail.com>
2020-09-30 12:17:55 -0300

    Add main without dns

---
+++ cmake-build-debug/CMakeFiles/FRC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FRC.dir/src/main.c.o"
+  "FRC"
+  "FRC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,7 +11,62 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(){
-    
+#define DNS_PORT 53
+#define NO_ATTEMPS 3
+#define BUFFER_LEN 4096
+
+int main(int argc, char **argv) {
+    /*
+     * argv[1]: Nome do domínio buscado
+     * argv[2]: IP do servidor DNS
+     * */
+
+
+    // Socket struct
+    struct sockaddr_in server;
+
+    // File descriptor for socket
+    int sockfd;
+
+    int len = sizeof(server);
+
+    // Receive buffer
+    char buffer_in[BUFFER_LEN];
+    // Send buffer
+    char buffer_out[BUFFER_LEN];
+
+    // Instantiate the socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Error on client socket creation:");
+        return EXIT_FAILURE;
+    }
+    printf("Socket file descriptor ID: %d\n", sockfd);
+
+    // Connection properties
+    server.sin_family = AF_INET;
+    server.sin_port = htons(DNS_PORT);
+    server.sin_addr.s_addr = inet_addr(argv[2]);
+    memset(server.sin_zero, 0x0, 8);
+
+    // Connection attempt
+    if (connect(sockfd, (struct sockaddr*) &server, len) == -1) {
+        perror("Can't connect to server");
+        return EXIT_FAILURE;
+    }
+
+    // Zeroing the buffers
+    memset(buffer_in, 0x0, BUFFER_LEN);
+    memset(buffer_out, 0x0, BUFFER_LEN);
+
+    fprintf(stdout, "Say something to the server: ");
+    fgets(buffer_out, BUFFER_LEN, stdin);
+
+    // Sends the read message to the server through the socket
+    send(sockfd, buffer_out, strlen(buffer_out), 0);
+
+    // Receives an answer from the server
+    recv(sockfd, buffer_in, BUFFER_LEN, 0);
+    printf("Server answer: %s\n", buffer_in);
+
     return 0;
 }
